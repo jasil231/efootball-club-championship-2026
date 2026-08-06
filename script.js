02c9bd74b52612af040a2980ecc7b9acb96eb8bc
jasil231 <jasil7269@gmail.com>
2026-08-06 18:28:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const topBtn = document.getElementById("topBtn");
 
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+};
+
+function topFunction() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+      }
